22d15ec13ec69a25e1e666defdcc3b0d52161813
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2021-08-29 18:58:14 +0430

    set structure of project

---
+++ src/views/App.jsx
@@ -0,0 +1,22 @@
+import "../assets/styles/index.scss";
+
+import React from "react";
+import Recipes from "./Recipes";
+
+import photo from "../assets/images/pic.jpg";
+function App() {
+  return (
+    <>
+      <section className="hero"></section>
+      <main>
+        <section>
+          <h1>I'm with React, mahdi</h1>
+        </section>
+        <img src={photo} alt="photo" width="250" />
+        <Recipes />
+      </main>
+    </>
+  );
+}
+
+export default App;
